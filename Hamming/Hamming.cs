26799321d69cbb1c35bcc3b3a82d3827d473e5a4
Kuldeep Sharma <kul123sharma@gmail.com>
2018-09-10 11:05:27 +0530

    Hamming-Solution-@hellokuldeep-1.1

--- Hamming/Hamming.cs
+++ Hamming/Hamming.cs
@@ -2,12 +2,21 @@
 
 namespace Hamming
 {
-    public class Hamming
-    {
-        public static int Distance(string original, string current)
-        {
+    public class Hamming{
+        public static int Distance(string original, string current){
             int hammingDistance = 0;
-            
+            if ( original == null ) { // Null string exception
+                    throw (new ArgumentNullException("original"));
+            } else if ( current == null ) {
+                    throw (new ArgumentNullException("current"));
+            }
+            if( original.Length != current.Length) {    // Exception for unequal string length
+                throw (new ArgumentException("Hamming Distance can only be calculated over strings of equal length"));
+            }
+            for (int i = 0; i < original.Length; i++){ // Length of Hamming
+                if(original[i] != current[i])
+                    hammingDistance++;
+            }
             return hammingDistance;
         }
     }
